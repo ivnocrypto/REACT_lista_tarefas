6ccdc5be14ba5015cc48f463298aa458d66b03a0
ivnogg <ivnocrypto@gmail.com>
2022-11-11 10:59:55 -0300

    componentes

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
   rules: {
     "prettier/prettier": 2,
     "react/jsx-filename-extension": 0,
+    "react/state-in-constructor": 0,
     "import/prefer-default-export": 0,
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
